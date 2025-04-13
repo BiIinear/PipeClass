139fcd6bad5938be733996c9444bf99aabffcc4b
BiIinear <tincannedtoxic@gmail.com>
2025-04-13 13:13:27 +0000

    Update PipeClass.lua

--- PipeClass.lua
+++ PipeClass.lua
@@ -1,119 +1,150 @@
-local RepStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
-
-local Players = game:GetService("Players")
-local Player = Players.LocalPlayer
-local TestGui = Player.PlayerGui:WaitForChild("TestGui")
 
 local PipeClass = {}
 PipeClass.__index = PipeClass
 
-PipeClass.START_RANGE = 0.0 -- Will be positive or negative
-PipeClass.TURN_ANGLE = 90.0 -- Will be positive or negative
-PipeClass.LENGTH_INCREMENT = 3.0
-PipeClass.TURN_CHANCE = 33.3 -- Write as 0.0 to 100.0, rolls dice per iteration
-PipeClass.TIME_SCALE = 5 -- The exponent to 2. Write as -1 to 10
+PipeClass.GIRTH = 2.0 -- The girth of the pipes
+PipeClass.TURN_ANGLE = 90.0 -- The degrees the pipes will turn
+PipeClass.LENGTH_INCREMENT = 3.0 -- How many studs the pipes will extend
+PipeClass.TURN_CHANCE = 33.3 -- Range 0 to 100, flips coin per step
+PipeClass.TIME_SCALE = 2^0 -- How many times the pipes will step per render stepped
 
-function PipeClass.new(brickColor: BrickColor?)
+function PipeClass.new(startCFrame: CFrame?, color: Color3?)
+	
+	local defaultColor = Color3.fromRGB(163, 162, 165) -- Medium stone grey
+	local defaultCFrame = CFrame.new()
+	local folderName = "Pipe"
+	
+	startCFrame = startCFrame or defaultCFrame
+	color = color or defaultColor
+	local vertexColor = Vector3.new(color.R, color.G, color.B) * Vector3.new(2, 2, 2)
 	
 	local self = setmetatable({}, PipeClass)
 	
-	self.Color = brickColor or BrickColor.Random()
-	
+	self.Color = color
+	self.VertexColor = vertexColor
+
 	self.Folder = Instance.new("Folder")
-	self.Folder.Name = tostring(self.Color).." pipe"
-	
-	self.InitNode = RepStorage.Node:Clone()
-	self.InitNode.CFrame = CFrame.new()
-	self.InitNode.Size *= Vector3.new(2, 2, 2)
-	self.InitNode.BrickColor = self.Color
+	self.Folder.Name = folderName
+
+	self.InitNode = instantiateNode()
+	self.InitNode.CFrame = startCFrame
+	self.InitNode.Mesh.VertexColor = self.VertexColor
 	self.InitNode.Parent = self.Folder
-	
-	self.InitEdge = RepStorage.Edge:Clone()
-	self.InitEdge.CFrame = CFrame.new()
-	self.InitEdge.BrickColor = self.Color
+
+	self.InitEdge = instantiateEdge()
+	self.InitEdge.CFrame = startCFrame * CFrame.new(PipeClass.GIRTH / 2.0, 0.0, 0.0)
+	self.InitEdge.Mesh.VertexColor = self.VertexColor
 	self.InitEdge.Parent = self.Folder
-	
+
 	self.ActiveNode = self.InitNode
 	self.ActiveEdge = self.InitEdge
 	self.Steps = 0
-	self.Surns = 0
-	
+	self.Turns = 0
+
 	return self
 end
 
-function PipeClass:Start(startCFrame: CFrame?)
-	
+function PipeClass:Start()
+
 	self.Folder.Parent = workspace
-	self.InitNode.CFrame = startCFrame or CFrame.new(randomStartPosition()) * CFrame.fromOrientation(randomRadian(), 0, randomRadian())
-	self.InitEdge.CFrame = self.InitNode.CFrame
-	
+
 	RunService.Stepped:Connect(function()
-		
-		for i = 1, 2^PipeClass.TIME_SCALE do
-			
-			self:Increment()
-			
-			if randomOutcome(PipeClass.TURN_CHANCE) then self:TurnPipe() end
-			
+
+		for i = 1, PipeClass.TIME_SCALE do
+
+			if flipCoin(PipeClass.TURN_CHANCE) then self:Turn() end
+			self:Step()
+
 			self.Steps += 1
 		end
 	end)
 end
 
-function PipeClass:Increment()
-	
-	self.ActiveEdge.Size += Vector3.new(PipeClass.LENGTH_INCREMENT, 0, 0)
-	self.ActiveEdge.CFrame *= CFrame.new(PipeClass.LENGTH_INCREMENT / 2, 0, 0)
+function PipeClass:Step()
+
+	self.ActiveEdge.Size += Vector3.new(PipeClass.LENGTH_INCREMENT, 0.0, 0.0)
+	self.ActiveEdge.Mesh.Scale = self.ActiveEdge.Size / Vector3.new(2.0, 2.0, 2.0)
+	self.ActiveEdge.CFrame *= CFrame.new(PipeClass.LENGTH_INCREMENT / 2.0, 0.0, 0.0)
 end
 
-function PipeClass:TurnPipe()
+function PipeClass:Turn()
 	
-	local NewNode = RepStorage.Node:Clone()
-	NewNode.CFrame = self:GetHeadCFrame() * CFrame.fromOrientation(randomRadian(), 0, randomRadian())
-	NewNode.BrickColor = self.Color
+	local randomSign = math.random(0, 1) == 1 and 1 or -1
+	local radian = math.rad(PipeClass.TURN_ANGLE) * randomSign
+	
+	local NewNode = instantiateNode()
+	NewNode.CFrame = self:GetHeadCFrame() * CFrame.fromOrientation(radian, 0.0, radian)
+	NewNode.Mesh.VertexColor = self.VertexColor
 	NewNode.Parent = self.Folder
-	
-	local NewEdge = RepStorage.Edge:Clone()
-	NewEdge.CFrame = NewNode.CFrame * CFrame.new(1, 0, 0)
-	NewEdge.BrickColor = self.Color
+
+	local NewEdge = instantiateEdge()
+	NewEdge.CFrame = NewNode.CFrame * CFrame.new(PipeClass.GIRTH / 2.0, 0.0, 0.0)
+	NewEdge.Mesh.VertexColor = self.VertexColor
 	NewEdge.Parent = self.Folder
-	
+
 	self.ActiveNode = NewNode
 	self.ActiveEdge = NewEdge
 	self.Turns += 1
 end
 
 function PipeClass:GetHeadCFrame(): CFrame
-	
-	local HeadCFrame = self.ActiveEdge.CFrame * CFrame.new(self.ActiveEdge.Size.X / 2, 0, 0)
-	
+
+	local HeadCFrame = self.ActiveEdge.CFrame * CFrame.new(self.ActiveEdge.Size.X / 2.0, 0.0, 0.0)
+
 	return HeadCFrame
 end
 
-function randomRadian(): number
+function instantiateNode(): Part
 	
-	local randomSign = math.random(0, 1) == 1 and 1 or -1
-	local radian = math.rad(PipeClass.TURN_ANGLE) * randomSign
+	local partScale = PipeClass.GIRTH
+	local meshScale = partScale / 20.0
 	
-	return radian
+	local Part = Instance.new("Part")
+	Part.Size = Vector3.new(partScale, partScale, partScale)
+	Part.CastShadow = false
+	Part.CanCollide = false
+	Part.CanTouch = false
+	Part.CanQuery = false
+	Part.Anchored = true
+	
+	local SpecialMesh = Instance.new("SpecialMesh")
+	SpecialMesh.MeshId = "rbxassetid://1111494591"
+	SpecialMesh.TextureId = "rbxassetid://12754303778"
+	SpecialMesh.Scale = Vector3.new(meshScale, meshScale, meshScale)
+	SpecialMesh.Parent = Part
+
+	return Part
 end
 
-function randomOutcome(desiredRange: number): boolean
+function instantiateEdge(): Part
 	
+	local partScale = PipeClass.GIRTH
+	local meshScale = partScale / 2.0
+	
+	local Part = Instance.new("Part")
+	Part.Size = Vector3.new(partScale, partScale, partScale)
+	Part.CastShadow = false
+	Part.CanCollide = false
+	Part.CanTouch = false
+	Part.CanQuery = false
+	Part.Anchored = true
+
+	local SpecialMesh = Instance.new("SpecialMesh")
+	SpecialMesh.MeshId = "rbxassetid://11698095403"
+	SpecialMesh.TextureId = "rbxassetid://12754303778"
+	SpecialMesh.Scale = Vector3.new(meshScale, meshScale, meshScale)
+	SpecialMesh.Parent = Part
+
+	return Part
+end
+
+function flipCoin(desiredRange: number): boolean
+
 	local possibleRange = math.random(1, 1000) / 10
 	local outcome = possibleRange <= desiredRange
-	
-	return outcome
-end
 
-function randomStartPosition(): Vector3
-	
-	local StartPosition = Vector3.new(math.random(-PipeClass.START_RANGE, PipeClass.START_RANGE), 1, 1)
-	StartPosition *= Vector3.new(1, math.random(-PipeClass.START_RANGE, PipeClass.START_RANGE), 1)
-	StartPosition *= Vector3.new(1, 1, math.random(-PipeClass.START_RANGE, PipeClass.START_RANGE))
-	
-	return StartPosition
+	return outcome
 end
 
 return PipeClass
